7a474bc8e3f900a5102627a26ad7de33b6a5570b
GabrielYuriRF0 <gabriel.yuri1020@gmail.com>
2023-07-21 11:11:03 -0300

    criação inicial do formulário

--- organograma/src/components/Banner/Banner.js
+++
@@ -1,7 +0,0 @@
-function Banner(){
-    return(
-        <img src="../../../public/images/banner.png"alt="Banner principal da página"/>
-    )
-}
-
-export default Banner
---
+++ organograma/src/components/TextLabel/index.js
@@ -0,0 +1,12 @@
+import './TextLabel.css'
+const TextLabel = () => {
+    return (
+        <div className='text-label'>
+            <label>Nome</label>
+            <input placeHolder='Digite seu nome'/>
+        </div>
+
+    )
+}
+
+export default TextLabel
